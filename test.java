96a56f920e4651e49d97beab25d6fb7104296e34
Hervadogoni <harvey.adgn@gmail.com>
2020-07-11 22:34:19 +0100

    third commit

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Test 2");
-        System.out.print("I'm testing");
+        System.out.println("I'm testing");
+        System.out.print("Again test");
     }
 }
